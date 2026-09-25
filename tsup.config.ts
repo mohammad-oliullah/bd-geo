e1440843b5537cc927752b58bf732cbb913dff8f
oliullah <mohammad.oliullah25@gmail.com>
2026-09-25 14:17:34 +0600

    chore: update package configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"], // ← add cjs
   dts: true,
   clean: true,
-  sourcemap: true,
 });
